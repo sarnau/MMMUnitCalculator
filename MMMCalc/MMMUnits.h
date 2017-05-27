9d1ed4418afe5692b926c88f701155b34a44e575
Markus Fritze <github@sarnau.com>
2017-05-28 00:00:14 +0200

    Comments

--- MMMCalc/MMMUnits.h
+++ MMMCalc/MMMUnits.h
@@ -10,15 +10,21 @@
 
 @interface MMMUnits : NSObject
 
-@property (readonly, class) MMMUnits *sharedUnits;     // A singleton, because we only want to load the unit file once
-@property (readonly) NSCharacterSet *unitCharacterset; // character set with all special characters from units to allow the parser to detect them without knowing them
-
-- (instancetype)initWithString:(NSString *)unitDataString NS_DESIGNATED_INITIALIZER;
+/// This allows loading a custom unit table via a NSURL
 - (instancetype)initWithURL:(NSURL *)unitURL;
+/// This allows loading a custom unit table from a string
+- (instancetype)initWithString:(NSString *)unitDataString NS_DESIGNATED_INITIALIZER;
 
+/// This will load a units.dat from the main bundle. A singleton, because we only want to load this file once.
+@property (readonly, class) MMMUnits *sharedUnits;
+
+/// Any parsing errors. This is typically only necessary for unit tests
 @property NSString *parseError;
 
-// Resolve a unit
+/// Resolve a unit
 - (NSString *)findUnit:(NSString *)theUnit;
+
+/// character set with all special characters from units to allow the parser to detect them without knowing them
+@property (readonly) NSCharacterSet *unitCharacterset;
 
 @end
